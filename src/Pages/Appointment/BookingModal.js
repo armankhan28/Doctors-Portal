47c70ff1fbec162095bb678284a7d49e68bf3b7e
captainkhan28 <86075671+captainkhan28@users.noreply.github.com>
2022-05-12 20:09:24 +0600

    commit

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,21 +1,40 @@
 import React from 'react';
+import { format } from 'date-fns';
 
-const BookingModal = ({treatment}) => {
-    const {name, slots} = treatment;
+const BookingModal = ({ date, treatment, setTreatment}) => {
+    const {_id, name, slots} = treatment;
+
+    const handleBooking = event =>{
+        event.preventDefault();
+        const slot = event.target.slot.value;
+        console.log(_id, name, slot);
+
+        // to close the modal
+        setTreatment(null);
+    }
+
   return (
     <div>
-      <input type="checkbox" id="booking-modal" class="modal-toggle" />
-      <div class="modal modal-bottom sm:modal-middle">
-        <div class="modal-box">
-          <h3 class="font-bold text-lg">
+      <input type="checkbox" id="booking-modal" className="modal-toggle" />
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">
             Booking for: {name}
           </h3>
-          <p class="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
-          <div class="modal-action">
-            <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+          <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 justify-items-center mt-2'>
+          <input type="text" disabled value={format(date, 'PP')} className="input input-bordered w-full max-w-xs" />
+          <select name="slot" className="select select-bordered w-full max-w-xs">
+                            {
+                                slots.map(slot =><option value={slot}>{slot}</option>)
+                            }
+          </select>
+          <input type="text" name="name" placeholder="Your Name" className="input input-bordered w-full max-w-xs" />
+          <input type="email" name="email" placeholder="Email Address" className="input input-bordered w-full max-w-xs" />
+          <input type="text" name="phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
+          <input type="submit" value="SUBMIT" className="btn btn-accent w-full max-w-xs" />
+          </form>
+          <div className="modal-action">
+            <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
           </div>
         </div>
       </div>
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -3,21 +3,21 @@ import React from 'react';
 const Service = ({ service, setTreatment }) => {
     const {name, slots} = service;
   return (
-    <div class="card w-96 bg-base-100 shadow-xl py-10">
-      <div class="card-body text-center">
-        <h2 class="text-xl font-bold text-secondary">{name}</h2>
+    <div className="card w-96 bg-base-100 shadow-xl py-10">
+      <div className="card-body text-center">
+        <h2 className="text-xl font-bold text-secondary">{name}</h2>
         <p>{
             slots.length > 0
             ? <span>{slots[0]}</span>
             : <span className='text-red-500'>Try another date.</span>
             }</p>
         <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'}</p>
-        <div class="card-actions justify-center">
+        <div className="card-actions justify-center">
           <label
-           for="booking-modal"
+           htmlFor="booking-modal"
            disabled={slots.length === 0} 
            onClick={() => setTreatment(service)}
-           class="btn btn-secondary text-white">Book Appointment</label>
+           className="btn btn-secondary text-white">Book Appointment</label>
         </div>
       </div>
     </div>
