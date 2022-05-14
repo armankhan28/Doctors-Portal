eb84dfa41f004e1c512d5f888cc6eec543bf5dcf
captainkhan28 <86075671+captainkhan28@users.noreply.github.com>
2022-05-14 20:03:22 +0600

    add loading

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from 'react-hook-form';
+import Loading from '../Shared/Loading';
 
 const Login = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm();
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+  const {register,formState: { errors },handleSubmit,} = useForm();
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
 
-  if (user) {
+  let signInError;
+
+  if(loading || gLoading){
+      return <Loading></Loading>
+  }
+
+  if(error || gError){
+      signInError= <p className='text-red-500'>{error?.message || gError?.message}</p>
+  }
+
+  if (gUser) {
     console.log(user);
   }
 
   const onSubmit = data => {
     console.log(data);
+    signInWithEmailAndPassword(data.email, data.password);
   };
 
   return (
@@ -77,6 +91,8 @@ const Login = () => {
                 
               </label>
             </div>
+
+            {signInError}
             <input className='btn w-full max-w-xs' type="submit" value="login" />
           </form>
           <div className="divider">OR</div>
